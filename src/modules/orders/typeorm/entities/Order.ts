955ae376cf663e42eaea76a08dfbd0b0a4c49c5d
Douglas Groff <100414633+DouglasGDev@users.noreply.github.com>
2023-09-16 16:27:01 -0400

    entidade da tabela pivo
    a tabela pivo é a que tem relacionamento com duas tabelas a de pedidos e a de produtos

--- src/modules/orders/typeorm/entities/Order.ts
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -1,5 +1,6 @@
-import { CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import Customer from "@modules/customers/typeorm/entities/Customer";
+import OrdersProducts from "./OrdersProducts";
 
 @Entity('orders')  // a entide comunica direto com a tabela orders do db
 class Order {
@@ -9,6 +10,11 @@ class Order {
   @ManyToOne(() => Customer) // o tipo de cardialidade que faz com o customer(cliente)
   @JoinColumn({name: 'customer_id'}) // e qual a coluna que faz a ligação da referencia
   customer: Customer;
+
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true,
+  })
+  order_products: OrdersProducts[];
 
   @CreateDateColumn()
   created_at: Date;
