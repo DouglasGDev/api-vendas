29397cc881c75e895af623157d7f1ce9fddf871c
Douglas Groff <100414633+DouglasGDev@users.noreply.github.com>
2023-09-16 15:54:14 -0400

    criando a entidade Orders
    essa entidade é responsável pela comunicação dos processos com a tabela referente nela e ali será criado o codigo do pedido pelo cliente.

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,20 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import Customer from "@modules/customers/typeorm/entities/Customer";
+
+@Entity('orders')  // a entide comunica direto com a tabela orders do db
+class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Customer) // o tipo de cardialidade que faz com o customer(cliente)
+  @JoinColumn({name: 'customer_id'}) // e qual a coluna que faz a ligação da referencia
+  customer: Customer;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Order;
