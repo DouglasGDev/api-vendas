278bcbf125b725c96fb06ecd9727e42d363c9b12
Douglas Groff <100414633+DouglasGDev@users.noreply.github.com>
2023-10-18 22:59:47 -0400

    criado diretorio onde salva foto de avatar, isso o local e o que vai para o amazon s3
    no caso somente o o local no momento.

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,9 +3,12 @@ import multer from "multer";
 import path from "path";
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads' ); // dirname é o diretório atual e os .. volta un nivel e por ultimo a pasta que quer deixar
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp' );// diretorio temporário, que vai servir pro arquivo ficar temporário e depois ser movido para ou aws s3 ou disco local
+
 
 export default {
   directory: uploadFolder, // diretorio definido
+  tmpFolder,
   storage: multer.diskStorage({ // aqui vai mexer com upload de fotos, arquivos o multer
     destination: uploadFolder, // para onde vai o upload
     filename(request, file, callback) {
